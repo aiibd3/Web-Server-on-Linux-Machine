fa807d6d06a4e94925994330d7e98f41731ad1da
Abdulrahman Hamoud <114152030+aiibd3@users.noreply.github.com>
2024-12-15 20:27:49 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,8 @@ $username = "web_user";
 $password = "StrongPassword123";
 $dbname = "web_db";
 
-// إنشاء الاتصال
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// التحقق من الاتصال
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
