b6e73647192d7fc3d21ba341394821011bb7c1c3
Abdulrahman Hamoud <a.hamoud6161@gmail.com>
2025-01-17 13:35:53 +0000

    Updated PHP code to use AWS Parameter Store for secure credentials

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
-<?php
-$servername = "localhost";
-$username = "web_user"; 
-$password = "StrongPassword123"; 
-$dbname = "web_db"; 
+require 'vendor/autoload.php';
 
+use Aws\Ssm\SsmClient;
+
+$ssm = new SsmClient([
+    'region' => 'your-region', // Replace with your AWS region, e.g., 'us-east-1'
+    'version' => 'latest'
+]);
+
+function getParameter($ssm, $name) {
+    $result = $ssm->getParameter([
+        'Name' => $name,
+        'WithDecryption' => true 
+    ]);
+    return $result['Parameter']['Value'];
+}
+
+$servername = getParameter($ssm, '/web_server/db_host'); 
+$username = getParameter($ssm, '/web_server/db_username');
+$password = getParameter($ssm, '/web_server/db_password'); 
+$dbname = getParameter($ssm, '/web_server/db_name'); 
+
+// Establish database connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
@@ -31,5 +48,4 @@ echo "<!DOCTYPE html>
 </html>";
 
 $conn->close();
-?>
 
